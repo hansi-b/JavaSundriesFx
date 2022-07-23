9b1196945d93d4491986853967522215e646a2de
Hans Bering <9629717+hansi-b@users.noreply.github.com>
2022-07-23 10:58:15 +0200

    added tests for validation + validated callback

--- sundriesFx/src/test/groovy/org/hansib/sundries/fx/TextFieldValidationSpec.groovy
+++ sundriesFx/src/test/groovy/org/hansib/sundries/fx/TextFieldValidationSpec.groovy
@@ -1,5 +1,7 @@
 package org.hansib.sundries.fx;
 
+import java.util.concurrent.atomic.AtomicReference
+
 import javafx.scene.Scene
 import javafx.scene.control.Button
 import javafx.scene.control.TextField
@@ -14,11 +16,14 @@ public class TextFieldValidationSpec extends AbstractAppSpec {
 	@Override
 	protected Scene createScene() {
 		button = new Button("enter")
-		textField = new TextFieldValidation().build()
+		textField = new TextField()
 		return new Scene(new HBox(textField, button))
 	}
 
 	def 'successful validation allows focus change'() {
+
+		given:
+		new TextFieldValidation(textField).build()
 
 		when:
 		clickOn(textField)
@@ -31,11 +36,68 @@ public class TextFieldValidationSpec extends AbstractAppSpec {
 
 	def 'failing default validation forces back focus'() {
 
+		given:
+		new TextFieldValidation(textField).build()
+
 		when:
 		clickOn(textField) // is empty
 		push(KeyCode.TAB)
 
 		then:
 		textField.focused
+	}
+
+	def 'can use custom validation'() {
+
+		given:
+		new TextFieldValidation(textField).withValidation(t -> "x".compareTo(t) < 0).build();
+
+		when:
+		clickOn(textField)
+		write("u")
+		push(KeyCode.TAB)
+
+		then:
+		textField.focused
+
+		when:
+		push(KeyCode.BACK_SPACE)
+		write("y")
+		push(KeyCode.TAB)
+
+		then:
+		button.focused
+	}
+
+	def 'validated callback is called when leaving field'() {
+
+		given:
+		AtomicReference<String> result = new AtomicReference("")
+		new TextFieldValidation(textField).withValidatedTextCallback(t -> result.set(t)).build();
+
+		when:
+		clickOn(textField) // is empty
+		write("hello world")
+		push(KeyCode.TAB)
+
+		then:
+		button.focused
+		result.get() == "hello world"
+	}
+
+	def 'validated callback is not called on validation failure'() {
+
+		given:
+		AtomicReference<String> result = new AtomicReference("")
+		new TextFieldValidation(textField).withValidation(t -> "x".compareTo(t) < 0).withValidatedTextCallback(t -> result.set(t)).build();
+
+		when:
+		clickOn(textField) // is empty
+		write("abc")
+		push(KeyCode.TAB)
+
+		then:
+		textField.focused
+		result.get() == ""
 	}
 }
