97281045818a605e2f6a5cd14c388fc88f48308a
Hans Bering <9629717+hansi-b@users.noreply.github.com>
2022-10-20 23:12:25 +0200

    added minimal tests for RenamableTab

---
+++ sundriesFx/src/test/groovy/org/hansib/sundries/fx/RenamableTabSpec.groovy
@@ -0,0 +1,49 @@
+package org.hansib.sundries.fx
+
+import javafx.application.Platform
+import javafx.scene.Scene
+import javafx.scene.control.TabPane
+import javafx.scene.input.KeyCode
+
+public class RenamableTabSpec extends AbstractAppSpec {
+
+	private TabPane tabPane
+	private RenamableTab renamableTab
+
+	@Override
+	protected Scene createScene() {
+		tabPane = new TabPane()
+
+		renamableTab = new RenamableTab('first text')
+		Platform.runLater(() -> {
+			tabPane.getTabs().add(renamableTab.getTab())
+		})
+		return new Scene(tabPane, 120, 40)
+	}
+
+	def 'can add renamable tab'() {
+
+		expect:
+		tabPane.getTabs()[0].graphic.text == 'first text'
+	}
+
+	def 'can edit tab text'() {
+
+		when:
+		doubleClickOn(queryNthTab(0))
+		write('Scooby Doo').type(KeyCode.ENTER)
+
+		then:
+		tabPane.getTabs()[0].graphic.text == 'Scooby Doo'
+	}
+
+	/**
+	 * from https://github.com/TestFX/TestFX/issues/634
+	 *
+	 * @param idx the zero-based index of the tab to be selected
+	 * @return the selected project tab - actually only the skin class
+	 */
+	def queryNthTab(idx) {
+		lookup(".tab-pane > .tab-header-area > .headers-region > .tab").nth(idx).query()
+	}
+}
