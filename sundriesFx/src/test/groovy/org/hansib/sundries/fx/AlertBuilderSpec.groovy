5a213a5823b2a06d3d06995f59a26050791d4ce8
Hans Bering <9629717+hansi-b@users.noreply.github.com>
2025-11-30 18:46:34 +0100

    add test for default button

--- sundriesFx/src/test/groovy/org/hansib/sundries/fx/AlertBuilderSpec.groovy
+++ sundriesFx/src/test/groovy/org/hansib/sundries/fx/AlertBuilderSpec.groovy
@@ -2,6 +2,7 @@ package org.hansib.sundries.fx
 
 import javafx.scene.control.Alert
 import javafx.scene.control.Alert.AlertType
+import javafx.scene.control.Button
 import javafx.scene.control.ButtonType
 import javafx.stage.Stage
 
@@ -15,14 +16,14 @@ public class AlertBuilderSpec extends ApplicationSpec {
 		FxToolkit.registerPrimaryStage()
 	}
 
-	def 'can build simple confirnmation dialogue'() {
+	def 'can build simple confirmation dialogue'() {
 
 		given:
 		Alert ab = null;
 		when:
 		interact(() -> {
 			ab = new AlertBuilder(AlertType.CONFIRMATION, 'Hello World') //
-					.withButton(ButtonType.OK, 'indeed').build()
+				.withButton(ButtonType.OK, 'indeed').build()
 		})
 
 		then:
@@ -30,5 +31,21 @@ public class AlertBuilderSpec extends ApplicationSpec {
 		ab.contentText == 'Hello World'
 		ab.buttonTypes == [ButtonType.OK]
 		ab.getDialogPane().lookupButton(ButtonType.OK).text == 'indeed'
+	}
+
+	def 'can set default button'() {
+
+		when:
+		Alert ab = null;
+		interact(() -> {
+			ab = new AlertBuilder(AlertType.CONFIRMATION, 'Hello World') //
+				.withButton(ButtonType.PREVIOUS, 'previous')
+				.withDefaultButton(ButtonType.NEXT, 'next')
+				.withButton(ButtonType.CANCEL, 'cancel').build()
+		})
+
+		then:
+		Button b = (Button) ab.getDialogPane().lookupButton(ButtonType.NEXT)
+		b.isDefaultButton()
 	}
 }
